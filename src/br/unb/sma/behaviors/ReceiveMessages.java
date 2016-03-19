c1083e313f5cad13f1359d3c857d7fe756637a6f
Denis Albuquerque <denisalbuquerque@gmail.com>
2016-03-18 23:41:13 -0300

    new Behaviour for agents that needs to receive messages

---
+++ src/br/unb/sma/behaviors/ReceiveMessages.java
@@ -0,0 +1,27 @@
+package br.unb.sma.behaviors;
+
+import br.unb.sma.utils.Utils;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+/**
+ * Created by zidenis.
+ * 17-03-2016
+ */
+public class ReceiveMessages extends CyclicBehaviour {
+
+    public static final String RCVD_MSG = "received-message";
+
+    @Override
+    public void action() {
+        ACLMessage msg = myAgent.receive();
+        if (msg != null) {
+            Utils.logInfo(myAgent.getLocalName() + " : mensagem recebida de " + msg.getSender().getLocalName());
+            getDataStore().put(RCVD_MSG, msg);
+            myAgent.doActivate();
+        } else {
+            // used to make the CyclicBehaviour be executed only when a new message is received
+            block();
+        }
+    }
+}
